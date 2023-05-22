eb3b84c4669efcf7887402d9a8eb2fdad6f0b98e
Sam-Mobed <masdebom@gmail.com>
2023-05-22 14:08:30 -0400

    worked on login and signup

--- routes/SignUp.js
+++ routes/SignUp.js
@@ -1,5 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require('mongoose');
+const userCollection = require('./schemas/User');
+
+function checkPassword(password){
+    if (password.length<10){
+        return false;
+    }
+    const regex = /(?=.*\d)(?=.*[\W])/; // Lookahead assertions for digit and special character
+    return regex.test(password);
+}
+
+function checkUsername(username){
+    if (username.length<6){
+        return false;
+    }
+    return true;
+}
 
 router.get("/", (req,res) => {
     try {
@@ -7,11 +24,44 @@ router.get("/", (req,res) => {
     } catch (e) {
         res.status(500).send("Server side error");
     }
+}).post("/", async (req,res) => {
+
+    const existingUser = await userCollection.findOne({username: req.body.username}); //we can't have this as await
+
+    if (existingUser){
+        res.status(400).send("Username already exists. Please choose a new one");
+    }
+
+    const newUser = {
+        username: req.body.username,
+        password: req.body.password,
+    };
+
+    const checkPword = checkPassword(req.body.password);
+    const checkUname = checkUsername(req.body.username);
+
+    if (!checkUname){
+        res.status(400).send("Username did not meet requirements");
+    }else if(!checkPword){
+        res.status(400).send("Password did not meet requirements");
+    }
+
+    //the eventListener we added in signup.js should prevent the user from inputing two different passwords
+    try{
+        await userCollection.insertOne(newUser); 
+        res.status(200).send("User created successfully."); //instead of send we have to redirect user to his homepage
+    } catch (error){
+        console.error("Error creating user: ", error); 
+        res.status(500).send("Server error occurred");
+    }
+    
 });
+//what we can do is check if username already exists, then we return an error
+//once the user has been added to the database, we send him to /userName/chatRooms
+//inside that dynamic route, we will check the chatRooms of the new user, which should be empty, so nothing is shown
 
 module.exports = router;
 
 //const mongoose
 
 //connect to mongoose, catch error, try again
-
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const express = require("express");
+const express = require('express');
 const mongoose = require('mongoose'); //the connection is established once here, and reused throughout application
+const userCollection = require('./schemas/User');
 const signupRouter = require('./routes/SignUp');
 
 const app = express();
@@ -8,8 +9,12 @@ const app = express();
 const port = process.env.port || 3000;
 //since we are writing all our views with ejs, view engine converts that code to html
 app.set('view engine', 'ejs');
-
 app.use("/signup", signupRouter);
+//we set a limit on the size of the JSON payload that can be parsed by middleware
+//this also makes it so any incoming JSOn request can be parsed and made available in req.body
+app.use(express.json({ limit: "100mb"}));
+app.use(express.urlencoded({extended:false})); //this parses URL-encoded data from incoming requests
+//NOTE: we have to make sure this doesn't conflict with encryption later
 
 //start off with a local database, later we will move it to mongoDB atlas
 const url = 'mongodb://localhost:27017/chatDatabase';
@@ -23,10 +28,6 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
     console.error('Failed to connect to MongoDB: ', error);
 });
 
-//we set a limit on the size of the JSON payload that can be parsed by middleware
-//this also makes it so any incoming JSOn request can be parsed and made available in req.body
-app.use(express.json({ limit: "100mb"}));
-
 //on index you have the login page.
 app.get("/", (req,res) => {
     try {
@@ -34,10 +35,18 @@ app.get("/", (req,res) => {
     } catch (e) {
         res.status(500).send("Server side error");
     }
+}).post("/", async (req,res) => {
+    //lookup user, if he doesnt exist throw error
+    //check passwords, if they don't match throw error
+    
+    const user = await userCollection.findOne({username: req.body.username});
+
+    if(user.password===req.body.password){
+        res.status(200).send("User authorized."); //if it works redirect him to homepage instead
+    } else {
+        res.status(400).send("Wrong Password");
+    }
 });
-
-app.use('/signup', signupRouter);
-
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`);
