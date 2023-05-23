c0b950e6282484fe2d5772193c60c0d43dec8a71
Sam-Mobed <masdebom@gmail.com>
2023-05-23 12:34:37 -0400

    worked on create.ejs and createRoom

--- routes/createRoom.js
+++ routes/createRoom.js
@@ -1,13 +1,19 @@
 const express = require("express");
-const router = express.Router();
+const router = express.Router({ mergeParams: true}); //mergeParams should give me access to :username from the parent router
 
 router.use(express.json({ limit: "100mb"})); //once again not sure why these are necessary inside any other file but server.js
 router.use(express.urlencoded({extended:false}));
 
 //once the user successfully creates the chatroom, store what's needed in the database and redirect his ass to the chatroom.
 router.get('/', async (req,res) => {
-    //render the page
+    try{
+        const url = `http://localhost:3000/users/${req.params.username}`; //for the form action
+        res.status(200).render('../views/create.ejs', {url: url});
+    } catch (e) {
+        res.status(500).send("Server side error");
+    }
 }).post(async (req,res) => {
+    const username = req.params.username; //we have to fetch the user from the database
     //create the chatroom and send the user now admin there.
 });
 
